37b0417063703b32f023af03857bdd16e52aa341
github username <github email address>
2018-11-15 17:56:14 -0500

    added formatting

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,21 @@
-# class CLI
-#
+class CLI
+
+
+
 
   def initialize
     @user = nil
     @rating = nil
+  end
+
+
+  def pastel
+    pastel = Pastel.new
+    pastel
+  end
+
+  def red_line
+    puts     pastel.red("====================================".center(70))
   end
 
   def welcome
@@ -13,8 +25,9 @@
     puts ""
 
     weekday = DateTime.now.strftime('%A')
-    puts "Happy #{weekday}!  I am Truly Unique <3.  Sign up or Log in"
-    prompt_user = puts "Sign up = S Login = L"
+    puts "Happy #{weekday}!  #{pastel.yellow("I am Truly Unique")} #{pastel.red("<3")}".center(90)
+    prompt_user = puts "Sign up = S Login = L".center(70)
+    red_line
     response = STDIN.gets.chomp
     until response.downcase == 'l' || response.downcase == 's'
       invalid_response
@@ -27,21 +40,24 @@
     elsif response.downcase == "s"
       sign_up
     end
+    red_line
   end
 
   def invalid_response
-    pastel = Pastel.new
-    puts pastel.red("Please put a valid response.")
+    puts pastel.red("Please put a valid response.").center(80)
   end
 
   def sign_up
-    puts "What's your name"
+    puts "What's your name".center(70)
+    red_line
     name = STDIN.gets.chomp
-    puts "What's your email address?"
+    puts "What's your email address?".center(70)
+    red_line
     email = STDIN.gets.chomp
 
     until new_email?(email)
-      puts "Email already exists.  Please enter a different email"
+      puts "Email already exists.  Please enter a different email".center(70)
+      red_line
       email = STDIN.gets.chomp
     end
       @user = User.create(name: name, email: email)
@@ -60,25 +76,31 @@
   end
 
   def get_ratings
-    puts "We love feedback! Please rate how this made you feel."
-    prompt_user = puts "On a scale of 1-5 \n
-    1 = Eh, maybe try writing better content. 5 = This is exactly what I needed!"
+    puts "We love feedback! Please rate how this made you feel.".center(70)
+    red_line
+    prompt_user = puts "On a scale of 1-5".center(70)
+    puts "1 = Eh, maybe try writing better content.".center(70)
+    puts "5 = This is exactly what I needed!".center(70)
+    red_line
     rating = STDIN.gets.chomp.to_i
     until rating >= 1 && rating <= 5
       invalid_response
       prompt_user
       rating = STDIN.gets.chomp.to_i
     end
-    puts "Thanks for your feedback! Keep flourishing (:"
+    puts "Thanks for your feedback! Keep flourishing (:".center(70)
+    red_line
     @rating = rating
   end
 
   def log_in
-    puts "What's your email address"
+    puts "What's your email address".center(70)
+    red_line
     email = STDIN.gets.chomp
 
     until search_email(email) != nil
-      puts "Please put a valid email"
+      puts "Please put a valid email".center(70)
+      red_line
       email = STDIN.gets.chomp
     end
     @user = User.find_by email: email
@@ -88,8 +110,10 @@
 
 
   def poem_or_quote
-    puts  "Would you like a poem or a quote?" #potentially put person's name in question
-    prompt_user = puts "Poem = P Quote = Q"
+    puts  "Would you like a poem or a quote?".center(70)
+    red_line
+    prompt_user = puts "Poem = P Quote = Q".center(70)
+    red_line
     poem_or_quote = STDIN.gets.chomp.downcase
     until poem_or_quote == 'p' || poem_or_quote == 'q'
       invalid_response
@@ -107,8 +131,10 @@
 
   def define_mood
 
-    puts "What kind of mood are you feeling today?" #potentially put person's name in question
-    prompt_user = puts "Encouragement = E Love = L Funny = F"
+    puts "What kind of mood are you feeling today?".center(70)
+    red_line
+    prompt_user = puts "Encouragement = E Love = L Funny = F".center(70)
+    red_line
     mood = STDIN.gets.chomp.downcase
     until mood == 'e' || mood == 'l' || mood == 'f'
       invalid_response
@@ -125,9 +151,6 @@
   end
 
   def greeting_return(email)
-    #search history for most recent entry
-    # what if they check the site and don't make an entry
-    # History.where("user_id = ? AND rating > ?", self.id, 3)
     user = User.where(email: email)[0]
     name = user.name
     `say "Welcome back #{name}!"`
@@ -135,9 +158,11 @@
     most_recent_letter = letters.last.letter_id
     mood = Letter.find(most_recent_letter).mood
     kind = Letter.find(most_recent_letter).kind
-    puts "Would you like to see your last #{mood} #{kind} again?"
+    puts "Would you like to see your last #{mood} #{kind} again?".center(70)
+    red_line
     `say "Would you like to see your last #{mood} #{kind} again?"`
-    prompt_user = puts "Y for Yes or N for No"
+    prompt_user = puts "Y for Yes or N for No".center(70)
+    red_line
     response = STDIN.gets.chomp
     until response.downcase == 'y' || response.downcase == 'n'
       invalid_response
@@ -145,7 +170,9 @@
       response = STDIN.gets.chomp
     end
     if response == "y"
-      Letter.find(most_recent_letter).content
+      puts Letter.find(most_recent_letter).content.center(70)
+      red_line
+      last_now_more
     elsif response == "n"
       post_login
     end
@@ -156,11 +183,34 @@
     kind = poem_or_quote
     mood = define_mood
     result = Letter.where(kind: kind, mood: mood).sample
-    puts result.content
+    puts result.content.center(70)
+    red_line
     get_ratings
 
     History.create(user_id: @user.id, letter_id: result.id, rating: @rating)
 
+  end
+
+  def last_now_more
+
+    weekday = DateTime.now.strftime('%A')
+
+    puts "Would you like to view a new letter now?".center(70)
+    red_line
+    prompt_user = puts "Y for Yes or N for No".center(70)
+    red_line
+    response = STDIN.gets.chomp
+    until response.downcase == 'y' || response.downcase == 'n'
+      invalid_response
+      prompt_user
+      response = STDIN.gets.chomp
+    end
+    if response == "y"
+      post_login
+    elsif response == "n"
+      puts "Happy #{weekday}! Keep flourishing #{pastel.red("(:")}".center(70)
+      red_line
+    end
   end
 
   def greeting_new(name)
@@ -172,7 +222,7 @@
 
 
   def hello
-    pastel = Pastel.new
+
 
     puts ""
     puts ""
@@ -196,6 +246,8 @@
 
       puts     pastel.yellow("start_your_day_on_a_positive_note".center(70))
 
+      # red_line
+
 
 
 
@@ -205,5 +257,5 @@
 
   end
 
-#
-# end
+
+end
